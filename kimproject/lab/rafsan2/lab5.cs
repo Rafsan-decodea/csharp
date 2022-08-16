32d1870f4eeb3a4f7d95863d35bba90d2e1ca16d
Rafsan <shazidno123@gmail.com>
2022-08-16 20:55:03 +0100

    lab 5 working

--- kimproject/lab/rafsan2/lab5.cs
+++ kimproject/lab/rafsan2/lab5.cs
@@ -19,6 +19,69 @@
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if(facultyeee.Checked)
+            {
+
+                string department = "";
+                if(departcse.Checked && departeee.Checked)
+                {
+                    department = department +" "+ departcse.Text + " AND " + departeee.Text;
+
+                }
+                else if(departcse.Checked)
+                {
+                    department = department + departcse.Text;
+                }
+                else if (departcse.Checked)
+                {
+                    department = department + departcse.Text;
+                }
+                else if (departeee.Checked)
+                {
+                    department = department + departeee.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Invalide Department");
+                }
+
+                 string data = "Faculty = EEE || Department" + department;
+                resultbox.Items.Add(data);
+            }
+
+            if (facultyme.Checked)
+            {
+
+                string department = "";
+                if (departme.Checked || departcfe.Checked || departipe.Checked || departte.Checked)
+                {
+                   if (departme.Checked)
+                    {
+                        department = department + " " + departme.Text;
+                    }
+                    if (departcfe.Checked)
+                    {
+                        department = department + " " + departcfe.Text;
+                    }
+                    if (departipe.Checked)
+                    {
+                        department = department + " " + departipe.Text;
+                    }
+                    if (departte.Checked)
+                    {
+                        department = department + " " + departte.Text;
+                    }
+
+                }
+               
+                else
+                {
+                    MessageBox.Show("Invalide Department");
+                }
+
+                string data = "Faculty = ME || Department" + department;
+                resultbox.Items.Add(data);
+            }
 
 
         }
